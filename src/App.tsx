8c1156c3f4aca5d20b5095477daafc872b43206d
MATEKUNA <kuna.mate@parlament.hu>
2023-09-25 14:52:31 +0200

    base routes, StoreItem, navlinks, base store

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
-import { Home } from './pages/Home'
-import { Store } from './pages/Store'
-import { About } from './pages/About'
-import { Navbar } from './components/Navbar'
-import { ShoppingCartProvider } from './context/ShoppingCartContext'
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return (
-    <ShoppingCartProvider>
-      <Navbar />
-      <Container className='mb-4'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/store' element={<Store />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </Container>
-    </ShoppingCartProvider>
-  )
+	return (
+		<div>
+			<Navbar />
+			<Container className="container">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/store" element={<Store />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</Container>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,18 +1,28 @@
-import { Col, Row } from 'react-bootstrap'
-import { StoreItem } from '../components/StoreItem'
-import storeItems from '../data/items.json'
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+import items from "../data/items.json";
 
 export function Store() {
-  return (
-    <>
-      <h1>Store</h1>
-      <Row md={2} xs={1} lg={3} className='g-3'>
+    return (
+        <>
+            <h1>Store</h1>
+            <Row md={2} xs={1} lg={3} className='g-3'>
+                {items.map((storeItem) => (
+                    <Col key={storeItem.id}>
+                        <StoreItem {...storeItem} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    );
+}
+
+{
+    /* <Row md={2} xs={1} lg={3} className='g-3'>
         {storeItems.map((item) => (
           <Col key={item.id}>
             <StoreItem {...item} />
           </Col>
         ))}
-      </Row>
-    </>
-  )
+      </Row> */
 }
